007dcfd800089dd0fed4d505190ac7beb28c8cf9
Mathis, DB9MAT <mathis@giev.de>
2017-01-20 18:33:43 +0100

    Initial STM32Pager support

--- src/transmitter/mod.rs
+++ src/transmitter/mod.rs
@@ -1,12 +1,14 @@
 pub mod dummy;
 pub mod audio;
 pub mod c9000;
+pub mod stm32pager;
 pub mod raspager;
 
 pub use self::dummy::DummyTransmitter;
 pub use self::audio::AudioTransmitter;
 pub use self::c9000::C9000Transmitter;
 pub use self::raspager::RaspagerTransmitter;
+pub use self::stm32pager::STM32Transmitter;
 
 use pocsag::Generator;
 
