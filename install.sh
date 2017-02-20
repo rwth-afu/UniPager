514a5698f63baa6ec4b20e459699d9f0993f39e5
Ralf Wilke <dh3wr@darc.de>
2017-02-20 10:58:32 +0000

    Improved install script & added update script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hint: Must be run as root!"
+echo "Hint: Must be run as root! Try sudo ./install.sh if not working."
 echo "Installing RustPager..."
 cp rustpager.service /etc/systemd/system/rustpager.service
 chown root /etc/systemd/system/rustpager.service
@@ -7,9 +7,13 @@ chmod 644 /etc/systemd/system/rustpager.service
 cp target/release/rustpager /usr/local/bin/rustpager
 chown root /usr/local/bin/rustpager
 chmod 744 /usr/local/bin/rustpager
-mkdir /etc/rustpager
+if [ ! -d "/etc/rustpager" ]; then
+  mkdir /etc/rustpager
+else
+  echo "Directory /etc/rustpager already present."
+fi
 sudo systemctl daemon-reload
-echo "Installation complete. Use -->sudo systemctl start rustpager <-- to start the service."
+echo "Installation complete. Use --> sudo systemctl start rustpager <-- to start the service."
 echo "Use --> sudo systemctl enable rustpager <-- to enable start at boot time."
-echo "Config-Interface is avaiable at http://URL:8073"
+echo "Config-Interface is avaiable at http://URLOfThisHost:8073"
 echo "Happy paging..."
